61f4a7dfa74f04949a2a9b36c810d9f90002958e
Pankaj Thakur <Pankaj.thakurdgp@gmail.com>
2020-01-20 19:07:55 +0530

    Updates

---
+++ precache-manifest.fce6a8bc2e64d4a05d8e7fe668383943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bb6a48ea8a5268eebc46b4d858ebc9",
+    "url": "/MonsterRoladex/index.html"
+  },
+  {
+    "revision": "a37c06d702e89f938026",
+    "url": "/MonsterRoladex/static/css/main.9f83d0db.chunk.css"
+  },
+  {
+    "revision": "f9e22d7cc6c4c351561a",
+    "url": "/MonsterRoladex/static/js/2.0b9f1bf0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/MonsterRoladex/static/js/2.0b9f1bf0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a37c06d702e89f938026",
+    "url": "/MonsterRoladex/static/js/main.5a9d2eff.chunk.js"
+  },
+  {
+    "revision": "339d883afc7c90396c05",
+    "url": "/MonsterRoladex/static/js/runtime-main.8707a584.js"
+  }
+]);
